c3479110732638ed68379f27e1e6ce378224ff98
Brachy <b0583286293@gmail.com>
2025-01-16 21:38:28 +0200

    with good package-json

--- config/DB.js
+++ config/DB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export async function connectToDB() {
     try {
-        let connection = await mongoose.connect(process.env.DB_URI||"mongodb://127.0.0.1:27017/myStore")
+        let connection = await mongoose.connect(process.env.DB_URI||"mongodb+srv://brachy222:B327640249b@cluster0.wu9xf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("mongo db connected" + connection.Connection)
     }
     catch (err) {
