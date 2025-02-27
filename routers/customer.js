2f967bbe0a950233e8bd62b86da101f682a20865
dvory weiss <d0583237262@gmail.com>
2025-02-27 17:04:46 +0200

    delete add

--- routers/customer.js
+++ routers/customer.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
 
 
-import { add, updatePassword, getAllCustomers, getById, update ,login,signUp} from "../controllers/customer.js"
+import {  updatePassword, getAllCustomers, getById, update ,login,signUp} from "../controllers/customer.js"
 
 const router = Router();
 
 router.get("/", getAllCustomers);
 router.get("/:id", getById);
 router.put("/:id", update);
-router.post("/", add);
 router.put("/:id",updatePassword)
 router.post('/login', login);
 router.post("/", signUp);
