20d8b6cb4d5eb2c4882ac5377572d61a0a22e274
dvory weiss <d0583237262@gmail.com>
2025-03-06 16:30:36 +0200

    Update to the update controller

--- controllers/product.js
+++ controllers/product.js
@@ -45,55 +45,38 @@ export async function deleteById(req, res) {
         res.status(400).json({ title: "cannot delete product", message: err.message })
     }
 }
-import mongoose from "mongoose";
-import productModel from "../models/productModel.js";
-
 export async function update(req, res) {
     let { body } = req;
     let { productName } = req.params;
 
-    // בדיקה אם שם המוצר קיים
-    if (!productName || productName.length < 2) {
-        return res.status(400).json({ title: "invalid productName", message: "Product name is required and must be at least 2 characters" });
-    }
-
-    if (body.price !== undefined && body.price < 0) {
-        return res.status(400).json({ title: "cannot update product", message: "Price must be greater than or equal to 0" });
-    }
-
-    if (body.ProductionDate) {
-        let productionDate = new Date(body.ProductionDate);
-        if (isNaN(productionDate.getTime())) {
-            return res.status(400).json({ title: "cannot update product", message: "Invalid production date format" });
-        }
-        if (productionDate > new Date()) {
-            return res.status(400).json({ title: "cannot update product", message: "Production date cannot be in the future" });
-        }
-    }
-
-    if (body.color && body.color.length < 3) {
-        return res.status(400).json({ title: "cannot update product", message: "Color must be at least 3 characters" });
-    }
-
-    if (body.size && body.size.length < 3) {
-        return res.status(400).json({ title: "cannot update product", message: "Size must be at least 3 characters" });
-    }
+    if (body.productName?.length <= 2)
+        return res.status(400).json({ title: "cannot update product", message: "name is too short" });
+    if (body.price < 0)
+        return res.status(400).json({ title: "cannot update product", message: "price must be more than 0" });
+    if (body.ProductionDate && new Date(body.ProductionDate) > new Date())
+        return res.status(400).json({ title: "cannot update product", message: "productionDate must not be after today" });
+    if (body.color && body.color.length < 3)
+        return res.status(400).json({ title: "cannot update product", message: "color must be more than 3 characters" });
+    if (body.size && body.size.length < 3)
+        return res.status(400).json({ title: "cannot update product", message: "size must be more than 3 characters" });
 
     try {
-        let updatedProduct = await productModel.findOneAndUpdate(
-            { productName: productName }, body, { new: true } 
-        );
+        let product = await productModel.findOne({ productName: productName });
 
-        if (!updatedProduct) {
-            return res.status(404).json({ title: "cannot update product", message: "No product found with the given name" });
-        }
+        if (!product)
+            return res.status(404).json({ title: "product not found", message: "No product found with this name" });
+
+        let updatedProduct = await productModel.findByIdAndUpdate(product._id, body, { new: true });
+
+        if (!updatedProduct)
+            return res.status(404).json({ title: "cannot update product", message: "No product found to update" });
 
         res.json(updatedProduct);
-    } 
-    catch (err) {
-        res.status(500).json({ title: "cannot update product", message: err.message });
+    } catch (err) {
+        res.status(400).json({ title: "cannot update product", message: err.message });
     }
 }
+
 
 export async function add(req, res) {
     let { body } = req;
