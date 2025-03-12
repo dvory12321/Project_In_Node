628cad689424a559f76e94a51028c289b1e2f5ed
dvory weiss <d0583237262@gmail.com>
2025-03-12 14:40:04 +0200

    Update generateToken.js

--- generateToken.js
+++ generateToken.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const generateToken = (user) => {
     return jwt.sign({ userName : user.userName,role: user.role },
