99492178ffb67e0623067d95a23347308444983d
Brachy <b0583286293@gmail.com>
2025-03-23 17:06:44 +0200

    with new in ObjectID

--- controllers/order.js
+++ controllers/order.js
@@ -100,7 +100,7 @@ export async function addOrder(req, res) {
         let newOrder = new orderModel({
             ...body,
             destDate: new Date(body.destDate),
-            cust_id: mongoose.Types.ObjectId(body.cust_id),
+            cust_id: new mongoose.Types.ObjectId(body.cust_id),
             date: new Date(),
             finallyPrice: (body.priceToShipment || 100) + totalPrice
         });
