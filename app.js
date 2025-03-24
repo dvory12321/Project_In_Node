9366763b725f92e895ff54f0b0a13d09447720a1
Brachy <b0583286293@gmail.com>
2025-03-24 12:49:18 +0200

    good key in token

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use("/api/customers", customerRouter)
 app.use("/api/orders", orderRouter)
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NDI4MTIzODEsImV4cCI6MTc0MjgxNTk4MX0.MKWLF641QwH5Z7_r2rGsNK1_T_rLxAC-phAEDFCKoi8";
-    const secretKey = "s1Ku36PCrmaPz5peM4MnhRTeK6FGC5eno1DcZ";
+    const secretKey = process.env.JWT_SECRET;
 
     try {
         const decoded = jwt.verify(token, secretKey);
