3c71b6dc69635b706ba7e872aef31d66195b8d95
Brachy <b0583286293@gmail.com>
2025-03-25 21:55:21 +0200

    without token in app

--- app.js
+++ app.js
@@ -22,20 +22,20 @@ app.use("/api/products", productRouter)
 app.use("/api/customers", customerRouter)
 app.use("/api/orders", orderRouter)
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IteR16jXm9eZIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzQyOTI1NDk1LCJleHAiOjE3NDI5MjkwOTV9.cRzJw2cc6W90ORMJgiGBC6U4wVEx4opwcI8Hk3W3Z34";
-    const secretKey = process.env.JWT_SECRET;
+// const token = ;
+//     const secretKey = process.env.JWT_SECRET;
 
-    try {
-        const decoded = jwt.verify(token, secretKey);
-        console.log("decoded: " , decoded);
-    } catch (err) {
-        console.error("פענוח הטוקן נכשל:", err);
-    }
+//     try {
+//         const decoded = jwt.verify(token, secretKey);
+//         console.log("decoded: " , decoded);
+//     } catch (err) {
+//         console.error("פענוח הטוקן נכשל:", err);
+//     }
 
-const port = process.env.PORT || 10000;
-app.listen(port, "0.0.0.0", ()=>{
-    console.log("app is running in port " + port)
-})
+// const port = process.env.PORT || 10000;
+// app.listen(port, "0.0.0.0", ()=>{
+//     console.log("app is running in port " + port)
+// })
 
 
     // טוקן ודוגמת מפתח
